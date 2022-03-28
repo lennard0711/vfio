de2b834fc8c44523cf5b9dbbee58e0b0ac71e12a
Lennard Indlekofer <info@lennard0711.eu>
2022-03-29 01:09:15 +0200

    enable cpu isolation

--- etc/libvirt/hooks/qemu.d/win10.sh
+++ etc/libvirt/hooks/qemu.d/win10.sh
@@ -3,11 +3,20 @@
 # Script for win10
 if [[ $1 == "win10" ]]; then
   if [[ $2 == "started" ]]; then
+    # CPU isolation
+    systemctl set-property --runtime -- user.slice AllowedCPUs=2,3,4,5,6,7,10,11,12,13,14,15
+    systemctl set-property --runtime -- system.slice AllowedCPUs=2,3,4,5,6,7,10,11,12,13,14,15
+    systemctl set-property --runtime -- init.scope AllowedCPUs=2,3,4,5,6,7,10,11,12,13,14,15
     # Disable DVI-D-0
     su -l lennard0711 -c "DISPLAY=:0 xrandr --output DVI-D-0 --off"
   fi
   if [[ $2 == "stopped" ]]; then
+    # Free all CPUs
+    systemctl set-property --runtime -- user.slice AllowedCPUs=0-15
+    systemctl set-property --runtime -- system.slice AllowedCPUs=0-15
+    systemctl set-property --runtime -- init.scope AllowedCPUs=0-15
     # Enable DVI-D-0
-    su -l lennard0711 -c "DISPLAY=:0 xrandr --output DVI-D-0 --auto --right-of HDMI-0"
+    su -l lennard0711 -c "DISPLAY=:0 xrandr --output DVI-D-0 --auto --left-of HDMI-0"
+    su -l lennard0711 -c "DISPLAY=:0 xrandr --output HDMI-0 --primary"
   fi
 fi
--- scripts/validate_iommu.sh
+++ scripts/validate_iommu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 shopt -s nullglob
-for g in `find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V`; do
+for g in $(find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V); do
     echo "IOMMU Group ${g##*/}:"
     for d in $g/devices/*; do
         echo -e "\t$(lspci -nns ${d##*/})"
